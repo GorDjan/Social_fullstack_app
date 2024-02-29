b2b027bfa83034c903c5a15a9634fa54ebc4ee03
GorDjan <gorduryan7@gmail.com>
2024-02-29 22:20:04 +0400

    all controllers done

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,14 @@
 const UserController = require('./user-controller')
+const CommentController = require('./comment-controller')
+const FollowController = require('./follow-controller')
+const LikeController = require('./like-controller')
+const PostController = require('./post-controller')
 
 
 module.exports = {
-	UserController
+	UserController,
+	CommentController,
+	FollowController,
+	LikeController,
+	PostController
 }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const UserController = require('../controllers/user-controller');
+const PostController = require("../controllers/post-controller");
+const FollowController = require("../controllers/follow-controller");
+const LikeController = require("../controllers/like-controller");
+const CommentController = require("../controllers/comment-controller");
 const { authenticateToken } = require('../middleware/auth');
 
 const uploadDestination = 'uploads'
@@ -16,13 +20,33 @@ const storage = multer.diskStorage({
 
 const uploads = multer({ storage: storage })
 
-
+//User routes
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/current', authenticateToken, UserController.current)
 router.get('/users/:id', UserController.getUserById)
 router.put('/users/:id', UserController.updateUser)
 
+// Post routes
+router.post("/posts", authenticateToken, PostController.createPost);
+router.get("/posts", authenticateToken, PostController.getAllPosts);
+router.get("/posts/:id", authenticateToken, PostController.getPostById);
+router.delete("/posts/:id", authenticateToken, PostController.deletePost);
 
+// follow routes
+router.post("/follow", authenticateToken, FollowController.followUser);
+router.delete("/unfollow/:id",authenticateToken, FollowController.unfollowUser);
+
+// like routes
+router.post("/likes", authenticateToken, LikeController.likePost);
+router.delete("/likes/:id", authenticateToken, LikeController.unlikePost);
+
+// comment routes
+router.post("/comments", authenticateToken, CommentController.createComment);
+router.delete(
+  "/comments/:id",
+  authenticateToken,
+  CommentController.deleteComment
+);
 
 module.exports = router;
